ff83966f6523d8b733a9322a5081ec145a84dfa7
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-19 14:34:42 +0100

    create spring boot app

---
+++ Microservices/quizapp/src/main/java/org/example/quizapp/dao/QuestionDao.java
@@ -0,0 +1,10 @@
+package org.example.quizapp.dao;
+
+import org.example.quizapp.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+}
---
+++ Microservices/quizapp/src/main/java/org/example/quizapp/services/QuestionService.java
@@ -0,0 +1,23 @@
+package org.example.quizapp.services;
+
+import org.example.quizapp.dao.QuestionDao;
+import org.example.quizapp.models.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    private final QuestionDao questionDao;
+
+    @Autowired
+    public QuestionService(QuestionDao questionDao) {
+        this.questionDao = questionDao;
+    }
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+}
