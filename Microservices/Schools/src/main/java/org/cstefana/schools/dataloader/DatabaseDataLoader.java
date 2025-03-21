04ce9d4341faceccae8e3392599c29e7624b9973
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-21 19:28:04 +0100

    create data loader with real data

--- Microservices/Schools/src/main/java/org/cstefana/schools/dataloader/DatabaseDataLoader.java
+++ Microservices/Schools/src/main/java/org/cstefana/schools/dataloader/DatabaseDataLoader.java
@@ -7,7 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+
 import java.util.Arrays;
 
 
