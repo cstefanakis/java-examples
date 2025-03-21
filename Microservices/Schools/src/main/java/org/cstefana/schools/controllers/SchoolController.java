cc2ebe285fc5d61aa517bf5fb38ce44b32e86292
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-21 20:18:42 +0100

    remove no necessary imports

--- Microservices/Schools/src/main/java/org/cstefana/schools/controllers/SchoolController.java
+++ Microservices/Schools/src/main/java/org/cstefana/schools/controllers/SchoolController.java
@@ -2,7 +2,6 @@ package org.cstefana.schools.controllers;
 
 import org.cstefana.schools.models.School;
 import org.cstefana.schools.services.SchoolService;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
