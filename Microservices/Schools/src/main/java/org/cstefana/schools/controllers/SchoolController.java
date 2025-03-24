3612b89659b9cef7c85041d794808442a29ec28a
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-24 12:14:31 +0100

    repair private to public

--- Microservices/Schools/src/main/java/org/cstefana/schools/controllers/SchoolController.java
+++ Microservices/Schools/src/main/java/org/cstefana/schools/controllers/SchoolController.java
@@ -19,33 +19,33 @@ public class SchoolController {
     }
 
     @GetMapping()
-    private ResponseEntity<List<School>> getAllSchools() {
+    public ResponseEntity<List<School>> getAllSchools() {
         return ResponseEntity.status(HttpStatus.OK).body(schoolService.getSchools());
     }
 
     @GetMapping("/{id}")
-    private ResponseEntity<School> getSchoolById(@PathVariable long id) {
+    public ResponseEntity<School> getSchoolById(@PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK).body(schoolService.getSchoolById(id));
     }
 
     @GetMapping("/school-name")
-    private ResponseEntity<School> getSchoolByName(@RequestParam String name) {
+    public ResponseEntity<School> getSchoolByName(@RequestParam String name) {
         return ResponseEntity.status(HttpStatus.OK).body(schoolService.getSchoolByName(name));
     }
 
     @PostMapping()
-    private ResponseEntity<School> createSchool(@RequestBody School school) {
+    public ResponseEntity<School> createSchool(@RequestBody School school) {
         return ResponseEntity.status(HttpStatus.CREATED).body(schoolService.createSchool(school));
     }
 
     @PutMapping("{id}")
-    private ResponseEntity<School> updateSchool(@PathVariable long id, @RequestBody School school) {
+    public ResponseEntity<School> updateSchool(@PathVariable long id, @RequestBody School school) {
         schoolService.updateSchool(id, school);
         return ResponseEntity.status(HttpStatus.OK).body(school);
     }
 
     @DeleteMapping("{id}")
-    private ResponseEntity<School> deleteSchool(@PathVariable long id) {
+    public ResponseEntity<School> deleteSchool(@PathVariable long id) {
         schoolService.deleteSchool(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
