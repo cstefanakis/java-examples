0ffc141b38cbf16cede90b93eeb0fb9eb3f4744e
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-25 09:17:12 +0100

    example with inner class

---
+++ oop/oop/src/main/java/org/cstefana/innerClass/Main.java
@@ -0,0 +1,36 @@
+package org.cstefana.innerClass;
+
+class A {
+    int age;
+        public void show()
+        {
+            System.out.println("in show");
+        }
+    class B {
+        public void config(){
+            System.out.println("in config");
+        }
+    }
+
+    static class C {
+        public void play(){
+            System.out.println("in play");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        //inner class call with this way.
+        A.B obj2 = obj.new B();
+        obj2.config();
+
+        //inner class if is static
+        A.C obj3 = new A.C();
+        obj3.play();
+
+    }
+}
